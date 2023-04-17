88aea27d1bcd5a7a887f0ff7a8d813691e7ae22c
MiraNizam <82273561+MiraNizam@users.noreply.github.com>
2023-04-17 08:51:37 +0300

    Add block try-except

--- get_schoolkid.py
+++ get_schoolkid.py
@@ -1,7 +1,11 @@
+from django.core.exceptions import ObjectDoesNotExist
 from datacenter.models import Schoolkid
 
 
 def get_schoolkid(kid_name):
-    schoolkid = Schoolkid.objects.get(full_name__contains=kid_name)
+    try:
+        schoolkid = Schoolkid.objects.get(full_name__contains=kid_name)
+    except Schoolkid.DoesNotExist:
+        print(f"Имени {kid_name} нет в базе данных учеников, попробуйте исправить и повторить попытку.")
     return schoolkid
 
