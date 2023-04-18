9d750f41f8effcd5e4fa007be1ba1e198c1048f4
MiraNizam <82273561+MiraNizam@users.noreply.github.com>
2023-04-18 10:06:09 +0300

    Add MultipleObjectsReturned

--- get_schoolkid.py
+++ get_schoolkid.py
@@ -1,4 +1,4 @@
-from django.core.exceptions import ObjectDoesNotExist
+from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
 from datacenter.models import Schoolkid
 
 
@@ -7,5 +7,9 @@ def get_schoolkid(kid_name):
         schoolkid = Schoolkid.objects.get(full_name__contains=kid_name)
     except Schoolkid.DoesNotExist:
         print(f"Имени {kid_name} нет в базе данных учеников, попробуйте исправить и повторить попытку.")
+        return
+    except Schoolkid.MultipleObjectsReturned:
+        print(f"Получено больше одного объекта")
+        return
     return schoolkid
 
